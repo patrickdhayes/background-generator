45d0ba53b7d01b14c5a1cc95dfeced2431a6afcd
patmandu <patmandu@github.com>
2018-10-05 23:25:11 +1300

    Adding Starting files for project

---
+++ script.js
@@ -0,0 +1,25 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+console.log(css, color1, color2);
+
+function changeColor () {
+    body.style.background = 
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+
+    console.log(body.style.background);
+    css.textContent = body.style.background + ";";
+}
+
+changeColor();
+
+color1.addEventListener("input", changeColor);
+
+color2.addEventListener("input", changeColor);
+
