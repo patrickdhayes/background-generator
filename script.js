e326f1f52528d4856e711b53309feb064e3b120b
patmandu <patmandu@github.com>
2018-10-13 20:08:07 +1300

    Adding changes to script css and index.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('anwswer: ', _.without(array, 5));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
